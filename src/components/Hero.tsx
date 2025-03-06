afe80d4b87a243f7f127fca067c12bd975c860df
jaysenjonsin <jasoncjohnson5@yahoo.com>
2025-03-06 10:25:51 -0800

    finish hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { Button } from './ui/button';
+import { ArrowDown, Github, Linkedin } from 'lucide-react';
 
 export function Hero() {
   const [typedText, setTypedText] = useState('');
@@ -27,9 +29,59 @@ export function Hero() {
           Hi, I'm <span className='text-primary'>Jason 👋</span>
         </h1>
         <h2 className='mb-8 text-2xl font-medium text-muted-foreground sm:text-3xl md:text-4xl'>
-          <span className='inline-block min-h-[1.5em]'>{typedText}</span>
+          <span className='inline-block min-h-[1.5em]'>
+            {typedText}
+            <span className='cursor-blink text-foreground'>|</span>
+          </span>
         </h2>
+        <p className='mx-auto mb-10 max-w-2xl text-lg text-muted-foreground'>
+          I build responsive, accessbile, and performant web applications with
+          modern technologies.
+        </p>
+        <div className='mb-12 flex justify-center space-x-4'>
+          <Button
+            asChild //this is to make the button a child of the link - instead of rendering <button> <a></a> </button> it will render as just <a> </a>
+            variant='outline'
+            size='icon'
+            className='rounded-full'
+          >
+            <a
+              href='https://github.com/jaysenjonsin'
+              target='_blank' //open in new tab
+              rel='noopener noreferrer' //security
+            >
+              <Github className='h-5 w-5' />
+              <span className='sr-only'>GitHub</span>
+            </a>
+          </Button>
+          <Button
+            asChild
+            variant='outline'
+            size='icon'
+            className='rounded-full'
+          >
+            <a
+              href='https://linkedin.com'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <Linkedin className='h-5 w-5' />
+              <span className='sr-only'>LinkedIn</span>
+            </a>
+          </Button>
+        </div>
+        <Button asChild>
+          <a href='#contact'>Get in touch</a>
+        </Button>
       </div>
+      <a
+        href='#about'
+        //use absolute positioning to position the arrow down icon in the center of the screen relative to the hero section
+        className='absolute bottom-8 left-1/2 flex -translate-x-1/2 animate-bounce items-center justify-center rounded-full p-2'
+        aria-label='Scroll to About section'
+      >
+        <ArrowDown className='h-6 w-6 text-muted-foreground' />
+      </a>
     </section>
   );
 }
