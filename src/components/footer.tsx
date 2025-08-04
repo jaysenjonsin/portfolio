57bcde02b4e45f949ad869d7490292ed1f1eee57
Jason Johnson <jasonjohnson@Jasons-MacBook-Air.local>
2025-08-03 18:35:00 -0700

    fix: remove comment

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,6 @@ export const Footer = () => {
   return (
     <footer className='w-full flex justify-center items-center py-4'>
       <p className='font-mono text-sm'>© 2025 Jason Johnson</p>
-      {/* testing push from local */}
     </footer>
   );
 };
